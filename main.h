251f4812374508bd649dad6c15c58f1f66978717
adoniskoco <10028@holbertonstudents.com>
2024-11-26 09:46:07 +0000

    added main.h and _putchar.c file

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+typedef struct specifier_t
+{
+	char *specifier;
+	int (*print)(va_list args);
+} specifier_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+int print_char(va_list args);
+int print_string(va_list args);
+int print_int(va_list args);
+
+#endif
