e077ab445cf79edbee49f2e0c9872e2f9a9954da
ordes <10027@holbertonstudents.com>
2024-11-27 09:25:53 +0000

    chamges

--- _printf.c
+++ _printf.c
@@ -17,13 +17,12 @@ int (*find_print(char format_char, specifier_t specifiers[]))(va_list)
 	return (NULL);
 }
 
-int process_format(const char *format, va_list args, specifier_t specifiers[])
-{
+
 	int process_format(const char *format, va_list args, specifier_t specifiers[])
 {
     unsigned int i = 0;
     int len = 0;
-    int (*handler)(va_list);
+    int (*print)(va_list);
     while (format[i] != '\0')
     {
         if (format[i] == '%' && format[i + 1] != '\0')
@@ -36,10 +35,10 @@ int process_format(const char *format, va_list args, specifier_t specifiers[])
             }
             else
             {
-                handler = find_handler(format[i + 1], specifiers);
-                if (handler)
+                print = find_print(format[i + 1], specifiers);
+                if (print)
                 {
-                    len += handler(args);
+                    len += print(args);
                     i++;
                 }
                 else
